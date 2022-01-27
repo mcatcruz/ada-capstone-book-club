10719dfd98a60488b1eea9ceef66b68ba80b8d3a
Monica Cruz <mcatcruz@gmail.com>
2022-01-27 12:49:02 -0800

    added env and .env to gitignore file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import dotenv
+from dotenv import load_dotenv, find_dotenv
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'adabookclub',
+        'USER': 'adabookclubuser',
+        'PASSWORD': 'adabookclubpassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
