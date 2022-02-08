f42dfe7a099f52323b1405fdea9ab0a384fbfbf6
Monica Cruz <mcatcruz@gmail.com>
2022-02-08 15:23:11 -0800

    Created Procfile, added django_heroku to settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ada-capstone-book-club.wsgi
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django-heroku
 from pathlib import Path
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -135,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
