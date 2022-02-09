dfceec5574f316614906be45c57768bafa425273
Monica Cruz <mcatcruz@gmail.com>
2022-02-08 17:09:20 -0800

    Added / to urls.py

--- backend_root/backend/urls.py
+++ backend_root/backend/urls.py
@@ -29,5 +29,6 @@ router.register(r'discussions', views.DiscussionView, 'discussion')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),   
-    path('adabookclub/', include(router.urls)),      
+    path('adabookclub/', include(router.urls)),  
+    path('/', include(router.urls))    
 ]
