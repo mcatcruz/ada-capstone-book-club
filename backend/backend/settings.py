6b41f1596f08e2db2303840cc2900ef4a08cbe2c
Monica Cruz <mcatcruz@gmail.com>
2022-02-08 16:43:50 -0800

    Edited INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'adabookclubapp',
+    'backend.adabookclubapp',
     'corsheaders',
     'rest_framework',
     'whitenoise.runserver_nostatic',
